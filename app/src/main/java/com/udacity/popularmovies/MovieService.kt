f56bf1ac22f686859d799d331aaeb55af1d51cd0
Dylan <accounts@dylan-hart.com>
2019-04-29 16:38:12 -0400

    Removed TODO for making queries safe for work; tMDB API does not explicitly support filtering by "adult" flag.

--- app/src/main/java/com/udacity/popularmovies/MovieService.kt
+++ app/src/main/java/com/udacity/popularmovies/MovieService.kt
@@ -11,8 +11,6 @@ import retrofit2.http.Query
 
 interface MovieService {
 
-    // TODO Ensure queries are safe for work.
-
     @GET("popular")
     fun requestPopularMovies(@Query("api_key") apiKey: String) : Call<Page>
 
