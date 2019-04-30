81d7a1875dfe63ba81798629eb111684a165040f
Dylan <accounts@dylan-hart.com>
2019-04-30 10:10:43 -0400

    Rewrite MovieDetails.kt as MovieDetails.java.

---
+++ app/src/main/java/com/udacity/popularmovies/data/MovieDetails.java
@@ -0,0 +1,53 @@
+package com.udacity.popularmovies.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import com.google.gson.annotations.SerializedName;
+
+public class MovieDetails implements Parcelable {
+
+    @SerializedName("budget")
+    private int budget;
+
+    @SerializedName("revenue")
+    private int revenue;
+
+    @SerializedName("runtime")
+    private int runtime;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(budget);
+        dest.writeInt(revenue);
+        dest.writeInt(runtime);
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+
+    public int getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(int revenue) {
+        this.revenue = revenue;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+}
