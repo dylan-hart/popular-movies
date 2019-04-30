a77f76d962d5e5117913ba084c1c2ac80a4fb41a
Dylan <accounts@dylan-hart.com>
2019-04-30 09:54:47 -0400

    Rewrote MovieService.kt as MovieService.java and RetroFitUtils.java.

---
+++ app/src/main/java/com/udacity/popularmovies/MovieService.java
@@ -0,0 +1,21 @@
+package com.udacity.popularmovies;
+
+import com.udacity.popularmovies.data.MovieDetails;
+import com.udacity.popularmovies.data.Page;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+interface MovieService {
+
+    @GET("popular")
+    Call<Page> requestPopularMovies(@Query("api_key")String apiKey);
+
+    @GET("top_rated")
+    Call<Page> requesetTopRatedMovies(@Query("api_key")String apiKey);
+
+    @GET("{id}")
+    Call<MovieDetails> requestMovieDetails(@Path("id")int id, @Query("api_key")String apiKey);
+
+}
